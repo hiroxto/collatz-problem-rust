70caba6c4747ab0b0a5c783537fb4db5a85a65aa
Hiroto Kitazawa <hiroxto@gmail.com>
2023-08-25 02:21:09 +0900

    feat: exit codeを設定

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::process::exit;
 
 fn collatz(n: u32, count: u32) -> (u32, u32) {
     if n == 1 {
@@ -22,12 +23,21 @@ fn start_collatz(n: i32) -> Result<(u32, u32), String> {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let input_number:i32= args[1].parse().unwrap();
+    let input_number: i32 = args[1].parse().unwrap();
 
     let result = start_collatz(input_number);
 
     match result {
-        Ok(v) => println!("指定された値: {}, 計算結果: {}, 回数: {}回", input_number, v.0, v.1),
-        Err(e) => println!("Error!: {}", e),
+        Ok(v) => {
+            println!(
+                "指定された値: {}, 計算結果: {}, 回数: {}回",
+                input_number, v.0, v.1
+            );
+            exit(0);
+        }
+        Err(e) => {
+            println!("Error!: {}", e);
+            exit(1);
+        }
     }
 }
