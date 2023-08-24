0553c4361bed1cf098ec16af522504f781c41bcc
Hiroto Kitazawa <hiroxto@gmail.com>
2023-08-25 02:11:02 +0900

    feat: コラッツの問題の計算処理を実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+fn collatz(n: u32, count: u32) -> (u32, u32) {
+    if n == 1 {
+        return (n, count);
+    }
+
+    let next = match n % 2 {
+        0 => n / 2,
+        _ => 3 * n + 1,
+    };
+    return collatz(next, count + 1);
+}
+
+fn start_collatz(n: i32) -> Result<(u32, u32), String> {
+    if n >= 1 {
+        Ok(collatz(n as u32, 1))
+    } else {
+        Err(String::from("数字には1以上を指定してください。"))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n: i32 = 11;
+    let result = start_collatz(n);
+
+    match result {
+        Ok(v) => println!("指定された値: {}, 計算結果: {}, 回数: {}回", n, v.0, v.1),
+        Err(e) => println!("Error!: {}", e),
+    }
 }
